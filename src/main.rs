95f23e3f0a8e339977a8f92b760d897d38d3c265
GOODBADBOY10 <selfmanademola@gmail.com>
2025-02-28 14:23:29 +0100

    Practise a crud operation with rust and firebase

---
+++ src/main.rs
@@ -0,0 +1,83 @@
+use std::collections::HashMap;
+
+use firebase_rs::*;
+use serde::{Deserialize, Serialize};
+
+#[derive(Serialize, Deserialize, Debug)]
+struct User {
+    name: String,
+    age: u32,
+    email: String
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+struct Response {
+    name: String,
+}
+
+#[tokio::main]
+async fn main() {
+    let user = User {
+        name: "Self Man".to_string(),
+        age: 30,
+        email: "selfmanademola@gmail.com".to_string(),
+    };
+
+    let firebase = Firebase::new("https://console.firebase.google.com/u/0/project/rust-92223/database/rust-92223-default-rtdb/data/~2F").unwrap();
+
+    let response = set_user(&firebase, &user).await;
+
+    let mut user = get_user(&firebase, &response.name).await;
+    println!("{:?}", user);
+
+    let users = get_users(&firebase).await;
+    println!("{:?}", users);
+
+
+    user.email = "updatedmail@gmail.com".to_sting();
+    let updated _user = update_user(&firebase, response.name, &user).await;
+    println!("{:?}", update_user);
+
+    delete_user(&firebase, &response.name).await;
+    println!("User deleted!");
+
+}
+
+async fn set_user(firebase_client: &Firebase, user: &User) -> Response {
+    let firebase = firebase_client.at("users");
+    let _users = firebase.set::<User>(&user).await;
+    return string_to_response(&_users.unwrap().data);
+}
+
+async fn get_users(firebase_client: &Firebase) -> HashMap<String, Users> {
+    let firebase = firebase_client.at("users");
+    let users = firebase.get::<HashMap<String, User>>().await;
+    println("{:?}", users);
+    return users.unwrap();
+}
+
+async fn get_user(firebase_client: &Firebase, id: &String) -> User {
+    let firebase = firebase_client.at("users").at(&id);
+    let user = firebase.get::<User>().await;
+    return user.unwrap();
+    println("{:?}", user);
+}
+
+async fn update_user(firebase_client: &Firebase, id: &String, user: &User) -> User {
+    let firebase = firebase_client.at("users").at(&id);
+    let _user = firebase.update::<User>.await;
+    return string_to_user(&_user.unwrap().data);
+}
+
+async fn delete_user(firebase_client: &Firebase, id: &String) {
+    let firebase = firebase_client.at("users").at(&id);
+    let _result = firebase.delete().await;
+}
+
+fn string_to_response(s: &str) -> Response {
+    serde_json::from_str(s).unwrap()
+}
+
+fn string_to_user(s: &str) -> User {
+    serde_json::from_str(s).unwrap()
+}
